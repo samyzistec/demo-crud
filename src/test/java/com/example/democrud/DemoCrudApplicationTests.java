85c6496aba358f99830c4bbd4d03ab7f20590c8e
Samuel Perez Zistecatl <samyzistec@gmail.com>
2024-08-27 01:36:52 -0600

    first commit

---
+++ src/test/java/com/example/democrud/DemoCrudApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.democrud;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class DemoCrudApplicationTests {
+
+	@org.junit.jupiter.api.Test
+	public void contextLoads() {
+	}
+
+}
